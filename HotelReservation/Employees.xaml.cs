d166f79ac8177cc4085a6a2aa1973030c7f8a514
Arth Thakkar <arththakkar@gmail.com>
2021-08-10 16:57:00 -0400

    Employees list display

--- HotelReservation/Employees.xaml.cs
+++ HotelReservation/Employees.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,7 +9,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Specialized;
+
 
 namespace HotelReservation
 {
@@ -19,9 +24,60 @@ namespace HotelReservation
     /// </summary>
     public partial class Employees : Window
     {
+
         public Employees()
         {
             InitializeComponent();
+            FillData();
+        }
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cnn"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter adp = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+        string sql = "";
+
+
+        public void FillData()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.Open();
+                sql = "Select * from Employee";
+                adp = new SqlDataAdapter(sql, con);
+                adp.Fill(dt);
+                if (dt.Rows.Count == 0)
+                {
+                    MessageBox.Show("Data not available...");
+                }
+                else
+                {
+                    //redirect to home page
+                    //MessageBox.Show("Login Done");
+                    List<DataRow> list = new List<DataRow>();
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        list.Add(dr);
+                    }
+
+                    //foreach (DataRow item in list)
+                    //{
+                    //    dg_employees.ItemsSource(item.ItemArray.ToString());
+                    //}
+                    dg_employees.ItemsSource = dt.AsEnumerable();
+                    
+                }
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.ToString());
+            }
         }
     }
 }
